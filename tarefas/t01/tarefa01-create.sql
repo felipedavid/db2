a6711187a5b62a85cbcc0fb982da31619a04b1f1
Felipe David <felipedavid.huh@gmail.com>
2022-05-01 13:21:49 -0300

    Atualizando questão 2

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -11,36 +11,36 @@ CREATE TABLE departamento (
     codigo SERIAL PRIMARY KEY,
     descricao VARCHAR(200),
     cod_gerente INT,
-	FOREIGN KEY(cod_gerente) REFERENCES funcionario(codigo)
+    FOREIGN KEY(cod_gerente) REFERENCES funcionario(codigo)
 );
 
 CREATE TABLE projeto (
     codigo SERIAL PRIMARY KEY,
     nome VARCHAR(50),
     descricao VARCHAR(200),
-    data_inicio DATE,
-    data_fim DATE,
 	cod_depto INT,
 	cod_responsavel INT,
-    CONSTRAINT fk_dep FOREIGN KEY(cod_depto) REFERENCES departamento(codigo),
-    CONSTRAINT fk_res FOREIGN KEY(cod_responsavel) REFERENCES funcionario(codigo)
+    data_inicio DATE,
+    data_fim DATE,
+    FOREIGN KEY(cod_depto) REFERENCES departamento(codigo),
+    FOREIGN KEY(cod_responsavel) REFERENCES funcionario(codigo)
 );
 
 CREATE TABLE atividade (
     codigo SERIAL PRIMARY KEY,
     nome VARCHAR(50),
     descricao VARCHAR(200),
+    cod_responsavel INT,
     data_inicio DATE,
     data_fim DATE,
-    cod_responsavel INT,
-	CONSTRAINT fk_dep FOREIGN KEY(cod_responsavel) REFERENCES funcionario(codigo)
+    FOREIGN KEY(cod_responsavel) REFERENCES funcionario(codigo)
 );
 
 CREATE TABLE atividade_projeto (
     cod_atividade INT,
     cod_projeto INT,
-	CONSTRAINT fk_atividade FOREIGN KEY(cod_atividade) REFERENCES atividade(codigo),
-	CONSTRAINT fk_projeto FOREIGN KEY(cod_projeto) REFERENCES projeto(codigo)
+    FOREIGN KEY(cod_atividade) REFERENCES atividade(codigo),
+    FOREIGN KEY(cod_projeto) REFERENCES projeto(codigo)
 );
 
 ALTER TABLE funcionario ADD CONSTRAINT fk_dep FOREIGN KEY(cod_departamento) REFERENCES departamento(codigo);
