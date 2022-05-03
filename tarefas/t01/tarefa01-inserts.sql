8648c325dd3cc651cad311c4628a364678595432
Felipe David <felipedavid.huh@gmail.com>
2022-05-03 08:56:47 -0300

    questão 3

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -6,29 +6,29 @@ INSERT INTO funcionario VALUES
 	(DEFAULT, 'marcos', 'm', '1998-11-11', 2000, DEFAULT);
 
 INSERT INTO departamento VALUES
-	(DEFAULT, 'Departamento 1', DEFAULT),
-	(DEFAULT, 'Departamento 2', DEFAULT),
-	(DEFAULT, 'Departamento 3', DEFAULT),
-	(DEFAULT, 'Departamento 4', DEFAULT),
-	(DEFAULT, 'Departamento 5', DEFAULT);
+	(DEFAULT, 'Departamento 1', 1),
+	(DEFAULT, 'Departamento 2', 3),
+	(DEFAULT, 'Departamento 3', 2),
+	(DEFAULT, 'Departamento 4', 2),
+	(DEFAULT, 'Departamento 5', 1);
 
 INSERT INTO projeto VALUES
-	(DEFAULT, 'Projeto 1', 'projeto X...', DEFAULT, DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'Projeto 2', 'projeto X...', DEFAULT, DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'Projeto 3', 'projeto X...', DEFAULT, DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'Projeto 4', 'projeto X...', DEFAULT, DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'Projeto 5', 'projeto X...', DEFAULT, DEFAULT, '1999-05-05', '2000-05-05');
+	(DEFAULT, 'Projeto 1', 'projeto X...', 3, 1, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'Projeto 2', 'projeto X...', 5, 1, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'Projeto 3', 'projeto X...', 2, 2, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'Projeto 4', 'projeto X...', 1, 2, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'Projeto 5', 'projeto X...', 3, 2, '1999-05-05', '2000-05-05');
 
 INSERT INTO atividade VALUES
-	(DEFAULT, 'terminar isso', 'fazer aquilo', DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'terminar isso', 'fazer aquilo', DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'terminar isso', 'fazer aquilo', DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'terminar isso', 'fazer aquilo', DEFAULT, '1999-05-05', '2000-05-05'),
-	(DEFAULT, 'terminar isso', 'fazer aquilo', DEFAULT, '1999-05-05', '2000-05-05');
+	(DEFAULT, 'terminar isso', 'fazer aquilo', 1, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'terminar isso', 'fazer aquilo', 3, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'terminar isso', 'fazer aquilo', 2, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'terminar isso', 'fazer aquilo', 1, '1999-05-05', '2000-05-05'),
+	(DEFAULT, 'terminar isso', 'fazer aquilo', 5, '1999-05-05', '2000-05-05');
 
 INSERT INTO atividade_projeto VALUES
-	(DEFAULT, DEFAULT),
-	(DEFAULT, DEFAULT),
-	(DEFAULT, DEFAULT),
-	(DEFAULT, DEFAULT),
-	(DEFAULT, DEFAULT);
+	(2, 3),
+	(1, 2),
+	(1, 5),
+	(3, 2),
+	(5, 1);
